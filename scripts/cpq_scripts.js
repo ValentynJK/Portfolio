36b8b2c09584c0ec4bbb02f6487b2c63e9ef06ed
Valentyn <valentynejk@gmail.com>
2022-01-13 22:11:20 +0100

    1

--- scripts/cpq_scripts.js
+++ scripts/cpq_scripts.js
@@ -10,7 +10,7 @@ const nextButton = document.querySelector('#nextButton');
 
 let counter = 1;
 const size = galleryImages[0].clientWidth; // returns width of the first image
-console.log(size)
+
 galleryImage.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 // Button listeners
@@ -20,3 +20,11 @@ nextButton.addEventListener('click', ()=> {
   counter++;
   galleryImage.style.transform = 'translateX(' + (-size * counter) + 'px)';
 })
+
+prevButton.addEventListener('click', ()=> {
+  galleryImage.style.transition = "transform 0.4s ease-in-out";
+  counter--;
+  galleryImage.style.transform = 'translateX(' + (-size * counter) + 'px)';
+})
+
+console.log(counter)
